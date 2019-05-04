1b81d0ce55e535ea20406e27c7230bd274445989
jalchr <jalchr@gmail.com>
2019-05-04 19:28:40 +0300

    Added pulse generator

--- raspberry.py
+++ raspberry.py
@@ -1,10 +1,28 @@
 from time import sleep
 from termcolor import colored, cprint
+import RepeatedTimer as scheduler
 
 balance = 0.0
+pulseGenerator = None
+pulses = 0
+
+def getPulses():
+    global pulses
+    pulses = pulses + 10
+
+def KW_meter_emulator():
+    # Sends pluses every millisecond
+    print("starting KW meter emulator...")
+    global pulseGenerator
+    # it auto-starts, no need of rt.start()  
+    pulseGenerator = scheduler.RepeatedTimer(1, getPulses) 
+
 
 def view_balance():
     cprint("Balance: " + str(balance), 'blue')
+
+def view_consumption():
+    cprint("Consumption: " + str(pulses), 'red')
 
 def refill(amount):
     global balance
@@ -26,23 +44,33 @@ def start_refill():
 
 def getCommand():
      text = input()
-     while text != 'r' and text != 'b' and text != 'q':        
-        cprint('Please enter valid commands: r => refill, b => view balance, q => quit', 'red')   
+     while text != 'r' and text != 'b' and text != 'q' and text != 'p':        
+        cprint('Please enter valid commands: r => refill, b => view balance, p => view consumption, q => quit', 'red')   
         text = input()
      return text
 
 def main():
     print("Initializing...")
-    sleep(2)    
+    KW_meter_emulator()
+
     cprint('Press any key to start', 'green', attrs=['blink'])   
-    cprint('Please enter valid commands: r => refill, b => view balance, q => quit', 'green')
+    cprint('Please enter valid commands: r => refill, b => view balance, p => view consumption, q => quit', 'green')
     
     command = getCommand()
     if command == 'r':
         start_refill()
     elif command == 'b':
         view_balance()
-    else:
+    elif command == 'p':
+        view_consumption()
+    elif command == 'q':
+       print("Quit")
        return
+
+    pulseGenerator.stop()
+    print("Stopping KW meter emulator")
+    print("")
+    print("-------------------------------------")
+    print("")
 
 main()
